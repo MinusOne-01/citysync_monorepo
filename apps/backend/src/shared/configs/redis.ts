bb9ac233ab5576d779432f4835b69d297f4a3fda
minusone <sgone2842@gmail.com>
2026-02-06 19:05:48 +0530

    deploy build v1

--- apps/backend/src/shared/configs/redis.ts
+++ apps/backend/src/shared/configs/redis.ts
@@ -7,3 +7,14 @@ export const redis = new Redis(REDIS_URL!, {
   maxRetriesPerRequest: null,
   enableReadyCheck: false
 });
+
+redis?.on("connect", () => {
+  console.log("Redis connected");
+  console.log("URL-> ", REDIS_URL)
+});
+
+redis.on("error", (err) => {
+  console.error("Redis error:", err.message);
+  console.log("URL-> ", REDIS_URL)
+});
+
