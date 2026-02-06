08f5eba15b76deb720488cd9d379c1135a5675c6
minusone <sgone2842@gmail.com>
2026-02-06 18:57:36 +0530

    deploy build v1

--- apps/backend/src/shared/configs/redis.ts
+++ apps/backend/src/shared/configs/redis.ts
@@ -3,7 +3,7 @@ import { env } from "./env";
 
 const REDIS_URL = env.REDIS_URL
 
-export const redis = new Redis(process.env.REDIS_URL!, {
+export const redis = new Redis(REDIS_URL!, {
   maxRetriesPerRequest: null,
   enableReadyCheck: false
 });
